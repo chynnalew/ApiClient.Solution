e489f26a69276bcf9d80555fd0def82e274740ae
chynnalew <chynnalew@yahoo.com>
2021-10-28 09:21:24 -0700

    create initial project
    Co-Authored-by: Paige Tiedeman <paigetiedeman@gmail.com>

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApiClient.Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
